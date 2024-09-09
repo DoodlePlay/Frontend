8c7aa94567f5806baf02756dd34260d8cd0bb04a
JeongMin_Byeon <bjm4256@gmail.com>
2024-09-09 18:25:43 +0900

    [FEAT] 공통 컴포넌트 이름표 #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import NamePlate from '../components/NamePlate';
+
 const HomePage = () => {
-  return <div className="text-black">Profile Setting Page</div>;
+  return (
+    <div className="text-black">
+      Profile Setting Page
+      <NamePlate title="test" score={200} drawing />
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/components/NamePlate.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface NamePlateProps {
+  title: string;
+  drawing?: boolean;
+  score?: number; // 점수가 있는 경우, 옵션으로 표시
+}
+
+const NamePlate: React.FC<NamePlateProps> = ({ title, score, drawing }) => {
+  return (
+    <div className="flex items-center justify-center bg-white rounded-5px shadow-lg max-w-xs p-5px border-2 border-neutral-default">
+      <div
+        className={`border-2 border-primary-default p-2 py-5px flex w-full rounded-5px items-center justify-center ${
+          drawing ? `flex-row gap-2.5` : 'flex-col'
+        }`}
+      >
+        <span
+          className={`text-black font-bold ${score ? 'text-base' : 'text-4xl'}`}
+        >
+          {title}
+        </span>
+        {score !== undefined && (
+          <span className="text-secondary-default text-base font-bold">
+            {score}점
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NamePlate;
