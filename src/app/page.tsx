17487da3f9e9d9702030ecf7155177ca125d7d83
JeongMin_Byeon <bjm4256@gmail.com>
2024-09-11 11:58:02 +0900

    [CHORE] pr후 수정 #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ const HomePage = () => {
   return (
     <div className="text-black">
       Profile Setting Page
-      <NamePlate title="test" score={200} drawing />
-      <SpeechBubble selectAvatar={false} title="Toxic Cover">
+      <NamePlate title="test" score={200} isDrawingActive />
+      <NamePlate title="선풍기" />
+      <SpeechBubble isAvatarSelected={false} title="Toxic Cover">
         군데군데 독극물을 뿌린다.
       </SpeechBubble>
-      <SpeechBubble selectAvatar />
+      <SpeechBubble isAvatarSelected />
     </div>
   );
 };
--- src/components/NamePlate.tsx
+++ src/components/NamePlate.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 interface NamePlateProps {
   title: string;
-  drawing?: boolean;
+  isDrawingActive?: boolean;
   score?: number; // 점수가 있는 경우, 옵션으로 표시
 }
 
-const NamePlate: React.FC<NamePlateProps> = ({ title, score, drawing }) => {
+const NamePlate: React.FC<NamePlateProps> = ({
+  title,
+  score,
+  isDrawingActive,
+}) => {
   return (
     <div className="flex items-center justify-center bg-white rounded-[5px] shadow-lg max-w-xs p-[5px] border-2 border-neutral-default">
       <div
-        className={`border-2 border-primary-default p-2 py-[5px] flex w-full rounded-[5px] items-center justify-center ${
-          drawing ? `flex-row gap-2.5` : 'flex-col'
+        className={`border-2 border-primary-default p-2 flex w-full rounded-[5px] items-center justify-center ${
+          isDrawingActive ? `flex-row gap-2.5 py-[5px]` : `flex-col py-[10px]`
         }`}
       >
         <span
