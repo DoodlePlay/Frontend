903c3824a181e24b53c9de55466527bd97ac38d5
jeongminDev <bjm4256@gmail.com>
2024-09-12 19:15:44 +0900

    [FEAT] 그림 툴바 개발 #8

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,4 +1,11 @@
+import NamePlate from '../../components/NamePlate/NamePlate';
+import Toolbar from '../../features/drawing/components/Toolbar';
+
 const GamePage = () => {
-  return <div>Playing Game Page</div>;
+  return (
+    <div>
+      <Toolbar />
+    </div>
+  );
 };
 export default GamePage;
