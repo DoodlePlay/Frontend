a25624b0ebb1e8276f28602c5dc74886d509ffe6
Dohyeong <kfh1001@gmail.com>
2024-09-19 14:29:51 +0900

    [FEAT] 로컬화상채팅, 좌우반전 기능 구현 #9

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef } from 'react';
+
+import useFlipStore from '../../features/profile/store/flipStore';
 
 interface AvatarProps {
   isVideoOn?: boolean;
@@ -21,6 +25,31 @@ const Avatar = ({
   size = 'small',
   src,
 }: AvatarProps) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const streamRef = useRef<MediaStream | null>(null);
+
+  const { isFlipped } = useFlipStore();
+  useEffect(() => {
+    if (isVideoOn && videoRef.current) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(stream => {
+          streamRef.current = stream;
+          videoRef.current!.srcObject = stream;
+          videoRef.current!.play();
+        })
+        .catch(err => {
+          console.error('Error accessing webcam: ', err);
+        });
+    } else {
+      if (streamRef.current) {
+        const tracks = streamRef.current.getTracks();
+        tracks.forEach(track => track.stop());
+        streamRef.current = null;
+      }
+    }
+  }, [isVideoOn]);
+
   return (
     <div
       className={`overflow-hidden outline outline-3 outline-black rounded-full ${
@@ -28,7 +57,13 @@ const Avatar = ({
       } ${isMyCharacter || isClicked ? 'bg-primary-default' : 'bg-white'}`}
     >
       {isVideoOn ? (
-        <video role="video"></video>
+        <video
+          ref={videoRef}
+          role="video"
+          className={`w-full h-full object-cover ${
+            isFlipped ? 'transform scale-x-[-1]' : ''
+          }`}
+        />
       ) : (
         <img
           src={src}
@@ -39,4 +74,5 @@ const Avatar = ({
     </div>
   );
 };
+
 export default Avatar;
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -4,11 +4,14 @@ import React from 'react';
 import { useState } from 'react';
 
 import ToggleButton from './ToggleButton';
+import useVideoStore from '../../features/profile/store/videoStore';
+import useFlipStore from '../../features/profile/store/flipStore';
+import useSoundStore from '../../features/profile/store/soundStore';
 
 const Settings = () => {
-  const [camera, setCamera] = useState(false);
-  const [sound, setSound] = useState(true);
-  const [flip, setFlip] = useState(false);
+  const { isVideoOn, toggleVideo } = useVideoStore();
+  const { isSoundOn, toggleSound } = useSoundStore();
+  const { isFlipped, toggleFlip } = useFlipStore();
   return (
     <div className="flex flex-col justify-between">
       <div className="flex items-center mb-[10px]">
@@ -17,15 +20,23 @@ const Settings = () => {
         <img
           src="/images/settings/cam-disabled.svg"
           alt="카메라 끄기"
-          className={`${!camera ? 'filter-none' : 'filter-grayscale  opacity-20 transition-all duration-300'}`}
+          className={`${
+            !isVideoOn
+              ? 'filter-none'
+              : 'filter-grayscale  opacity-20 transition-all duration-300'
+          }`}
         />
         <div className="mx-[30px]">
-          <ToggleButton isOn={camera} onToggle={() => setCamera(!camera)} />
+          <ToggleButton isOn={isVideoOn} onToggle={toggleVideo} />
         </div>
         <img
           src="/images/settings/cam.svg"
           alt="카메라 켜기"
-          className={`${camera ? 'filter-none' : 'filter-grayscale  opacity-20 transition-all duration-300'}`}
+          className={`${
+            isVideoOn
+              ? 'filter-none'
+              : 'filter-grayscale  opacity-20 transition-all duration-300'
+          }`}
         />
       </div>
       <div className="flex items-center mb-[10px]">
@@ -34,21 +45,29 @@ const Settings = () => {
         <img
           src="/images/settings/speaker-none.svg"
           alt="효과음 끄기"
-          className={`${!sound ? 'filter-none' : 'filter-grayscale  opacity-20 transition-all duration-300'}`}
+          className={`${
+            !isSoundOn
+              ? 'filter-none'
+              : 'filter-grayscale  opacity-20 transition-all duration-300'
+          }`}
         />
         <div className="mx-[30px]">
-          <ToggleButton isOn={sound} onToggle={() => setSound(!sound)} />
+          <ToggleButton isOn={isSoundOn} onToggle={toggleSound} />
         </div>
         <img
           src="/images/settings/speaker.svg"
           alt="효과음 켜기"
-          className={`${sound ? 'filter-none' : 'filter-grayscale  opacity-20 transition-all duration-300'}`}
+          className={`${
+            isSoundOn
+              ? 'filter-none'
+              : 'filter-grayscale  opacity-20 transition-all duration-300'
+          }`}
         />
       </div>
       <div className="flex items-center mb-[10px]">
         <img src="/images/settings/gear.svg" alt="이미지" />
         <span className="text-2xl font-bold ml-1 mr-3">카메라 좌우 반전</span>
-        <ToggleButton isOn={flip} onToggle={() => setFlip(!flip)} />
+        <ToggleButton isOn={isFlipped} onToggle={toggleFlip} />
       </div>
     </div>
   );
