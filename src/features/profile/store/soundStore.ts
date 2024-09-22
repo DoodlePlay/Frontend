9a9e24fadf44bb434b406cf0637a15d770fa682f
Dohyeong <kfh1001@gmail.com>
2024-09-22 16:03:15 +0900

    [FIX] 효과음, 좌우반전 상태를 로컬스토리지에 저장하지않도록 수정 #9

--- src/features/profile/store/soundStore.ts
+++ src/features/profile/store/soundStore.ts
@@ -3,18 +3,11 @@ import { create } from 'zustand';
 interface SoundState {
   isSoundOn: boolean;
   toggleSound: () => void;
-  setSoundState: (value: boolean) => void; //상태를 직접 설정
 }
 
 const useSoundStore = create<SoundState>(set => ({
   isSoundOn: true,
-  toggleSound: () =>
-    set(state => {
-      const newValue = !state.isSoundOn;
-      localStorage.setItem('sound-storage', JSON.stringify(newValue)); // local storage에 저장
-      return { isSoundOn: newValue };
-    }),
-  setSoundState: (value: boolean) => set({ isSoundOn: value }), // 상태를 local storage에서 불러온 값으로 설정
+  toggleSound: () => set(state => ({ isSoundOn: !state.isSoundOn })),
 }));
 
 export default useSoundStore;
