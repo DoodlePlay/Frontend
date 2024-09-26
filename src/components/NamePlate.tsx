7065c7782c8ee346c1bbb904529a88d9f8a1d33c
Dohyeong <kfh1001@gmail.com>
2024-09-26 18:36:20 +0900

    [FEAT] NamePlate 컴포넌트 임의 수정 #17

--- src/components/NamePlate.tsx
+++ src/components/NamePlate.tsx
@@ -12,17 +12,13 @@ const NamePlate: React.FC<NamePlateProps> = ({
   isDrawingActive,
 }) => {
   return (
-    <div className="flex items-center justify-center bg-white rounded-[5px] shadow-lg max-w-xs p-[5px] border-2 border-neutral-default">
+    <div className="min-w-[100px] flex-shrink flex items-center justify-center bg-white rounded-[5px] shadow-lg max-w-xs p-[5px] border-2 border-neutral-default">
       <div
-        className={`border-2 border-primary-default p-2 flex w-full rounded-[5px] items-center justify-center ${
-          isDrawingActive ? `flex-row gap-2.5 py-[5px]` : `flex-col py-[10px]`
+        className={`border-2 border-primary-default p-1 flex w-full rounded-[5px] items-center justify-center ${
+          isDrawingActive ? `flex-row gap-2.5 px-[8px] py-[5px]` : `flex-col`
         }`}
       >
-        <span
-          className={`text-black font-bold ${score ? 'text-base' : 'text-4xl'}`}
-        >
-          {title}
-        </span>
+        <span className={'text-black font-bold  text-[14px]'}>{title}</span>
         {score !== undefined && (
           <span className="text-secondary-default text-base font-bold">
             {score}점
