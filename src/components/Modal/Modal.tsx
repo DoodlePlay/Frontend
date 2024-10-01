7325f327abb2789d3b02133f2276acbb30924a2a
sumin-dev <bonar8888@naver.com>
2024-10-02 02:38:43 +0900

    [FEAT] Modal의 외부를 클릭해도 닫히도록 수정 #13

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 
 interface ModalProps {
@@ -12,9 +10,23 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, title, children, onClose }) => {
   if (!isOpen) return null;
 
+  const onClickBackground = (e: React.MouseEvent) => {
+    onClose();
+  };
+
+  const onClickModal = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/[.13] z-10">
-      <div className="flex flex-col gap-5 bg-white w-[500px] p-[30px] rounded-2xl border-[3px] border-black drop-shadow-modal">
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black/[.13] z-10"
+      onClick={onClickBackground}
+    >
+      <div
+        className="flex flex-col gap-5 bg-white w-[500px] p-[30px] rounded-2xl border-[3px] border-black drop-shadow-modal"
+        onClick={onClickModal}
+      >
         <div className="flex justify-between">
           <h2 className="text-2xl font-semibold">{title}</h2>
           <button onClick={onClose} aria-label="Close modal">
