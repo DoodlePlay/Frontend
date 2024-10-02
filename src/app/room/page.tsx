89d66d75a4ce6991d5484213625fa439af75eb71
sumin-dev <bonar8888@naver.com>
2024-10-02 11:04:52 +0900

    [REFACTOR] rooms page에서 컴포넌트 분리 #13

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,32 +1,15 @@
-'use client';
+import CreateRoomButton from '../../features/lobby/components/CreateRoomButton';
+import RoomSearchSection from '../../features/lobby/components/RoomSearchSection';
+import { getRooms } from '../../features/lobby/api/gameRoomsApi';
 
-import Button from '../../components/Button/Button';
-import RefreshButton from '../../features/lobby/components/RefreshButton';
-import RoomList from '../../features/lobby/components/RoomList';
-import SearchBar from '../../features/lobby/components/SearchBar';
-
-const LobbyPage = () => {
-  const onRefreshRoomList = async () => {
-    // RoomList를 새로 가져오는 비동기 작업
-    await new Promise(resolve => setTimeout(resolve, 3000)); // 3초 동안 지연
-  };
+const LobbyPage: React.FC = async () => {
+  const rooms = await getRooms();
 
   return (
     <div className="flex gap-10 w-[1200px] mt-[51px] p-[30px] rounded-[20px] bg-white shadow-board mx-auto">
-      <div className="flex flex-col gap-10 w-[720px]">
-        <div className="flex gap-5 items-center justify-between">
-          <SearchBar />
-          <RefreshButton onClick={onRefreshRoomList} />
-        </div>
-        <RoomList />
-      </div>
+      <RoomSearchSection rooms={rooms} />
       <div className="flex flex-col gap-10 w-[380px]">
-        <Button
-          text="Create"
-          color="primary"
-          onClick={() => {}}
-          className="h-[60px]"
-        />
+        <CreateRoomButton />
         {/* 도형님 수정 필요 */}
         <div className="p-[30px] rounded-[10px] border-4 border-black h-[440px]">
           User Box
@@ -35,4 +18,5 @@ const LobbyPage = () => {
     </div>
   );
 };
+
 export default LobbyPage;
