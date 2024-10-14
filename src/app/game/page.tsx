8424eafcb387f1e299c4153c7a6ced401a435267
Dohyeong <kfh1001@gmail.com>
2024-10-14 16:45:57 +0900

    [FEAT] game 페이지 ProtectedRoute 설정 #17

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -6,25 +6,27 @@ import VideoChat from '../../features/videoChat/components/VideoChat';
 
 const GamePage = () => {
   return (
-    <div className="flex items-center justify-center min-h-dvh overflow-hidden scrollbar-hide">
-      <div className="flex w-full max-w-[1240px] gap-[40px]">
-        <div className="flex flex-col left w-full gap-y-[20px]">
-          <Drawing />
-          <div className="w-full h-[240px] bg-disabled border-[4px] border-black rounded-[10px]">
-            chat
+    <ProtectedRoute>
+      <div className="flex items-center justify-center min-h-dvh overflow-hidden scrollbar-hide">
+        <div className="flex w-full max-w-[1240px] gap-[40px]">
+          <div className="flex flex-col left w-full gap-y-[20px]">
+            <Drawing />
+            <div className="w-full h-[240px] bg-disabled border-[4px] border-black rounded-[10px]">
+              chat
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col right w-full max-w-[420px] gap-y-[20px]">
-          <div className="w-full border-[4px] border-black rounded-[10px]">
-            <VideoChat />
-          </div>
-          <div className="flex flex-col w-full gap-y-[20px]">
-            <ItemBox />
-            <GameControlButtons />
+          <div className="flex flex-col right w-full max-w-[420px] gap-y-[20px]">
+            <div className="w-full border-[4px] border-black rounded-[10px]">
+              <VideoChat />
+            </div>
+            <div className="flex flex-col w-full gap-y-[20px]">
+              <ItemBox />
+              <GameControlButtons />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 };
 export default GamePage;
