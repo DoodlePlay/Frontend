cfe34db22e3004e8e985b9e6a130807177939b3e
sumin-dev <bonar8888@naver.com>
2024-10-16 03:18:58 +0900

    [FEAT] 대기실에서 방만들기 소켓 연결 #20

--- src/features/lobby/components/CreateRoomModal.tsx
+++ src/features/lobby/components/CreateRoomModal.tsx
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs';
 import Modal from '../../../components/Modal/Modal';
 import Button from '../../../components/Button/Button';
 import { createRoom } from '../api/gameRoomsApi';
+import socketStore from '../../socket/socketStore';
 
 interface RoomFormValues {
   roomTitle: string;
@@ -22,6 +23,7 @@ const CreateRoomModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
   onClose,
 }) => {
   const router = useRouter();
+  const { connectSocket } = socketStore();
   const {
     register,
     handleSubmit,
@@ -61,9 +63,9 @@ const CreateRoomModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
 
     try {
       const roomId = await createRoom(roomData);
-      console.log('Room created with ID:', roomId);
+      connectSocket(roomId);
       onClose();
-      router.push('/game'); // 추후 roomId 붙은 라우터 수정
+      router.push('/game');
     } catch (error) {
       console.error('Error creating room:', error);
     }
