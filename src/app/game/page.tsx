1a8b7ec03c9b710bc912bdeeaf1553c5d9f15239
jeongminDev <bjm4256@gmail.com>
2024-10-16 22:32:43 +0900

    [CHORE] 주석 해제 #23

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -12,27 +12,27 @@ const GamePage = () => {
   const [activeItem, setActiveItem] = useState<string | null>(null);
 
   return (
-    // <ProtectedRoute>
-    <div className="flex items-center justify-center min-h-dvh overflow-hidden scrollbar-hide">
-      <div className="flex w-full max-w-[1240px] gap-[40px]">
-        <div className="flex flex-col left w-full gap-y-[20px]">
-          <Drawing activeItem={activeItem} />
-          <div className="w-full h-[240px] bg-disabled border-[4px] border-black rounded-[10px]">
-            chat
+    <ProtectedRoute>
+      <div className="flex items-center justify-center min-h-dvh overflow-hidden scrollbar-hide">
+        <div className="flex w-full max-w-[1240px] gap-[40px]">
+          <div className="flex flex-col left w-full gap-y-[20px]">
+            <Drawing activeItem={activeItem} />
+            <div className="w-full h-[240px] bg-disabled border-[4px] border-black rounded-[10px]">
+              chat
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col right w-full max-w-[420px] gap-y-[20px]">
-          <div className="w-full border-[4px] border-black rounded-[10px]">
-            <VideoChat />
-          </div>
-          <div className="flex flex-col w-full gap-y-[20px]">
-            <ItemBox onItemClick={setActiveItem} />
-            <GameControlButtons />
+          <div className="flex flex-col right w-full max-w-[420px] gap-y-[20px]">
+            <div className="w-full border-[4px] border-black rounded-[10px]">
+              <VideoChat />
+            </div>
+            <div className="flex flex-col w-full gap-y-[20px]">
+              <ItemBox onItemClick={setActiveItem} />
+              <GameControlButtons />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    // </ProtectedRoute>
+    </ProtectedRoute>
   );
 };
 export default GamePage;
