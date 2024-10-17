592b51a62a2a5897b14daef78afa1381a912876a
jeongminDev <bjm4256@gmail.com>
2024-10-17 20:13:29 +0900

    [REFACTOR] 타임바 수정 #23

--- src/features/drawing/components/TimeBar.tsx
+++ src/features/drawing/components/TimeBar.tsx
@@ -6,7 +6,7 @@ interface TimerBarProps {
   isTimeCut: boolean;
 }
 
-const MIN_DURATION = 1; // 최소 지속 시간 1초
+const MIN_DURATION = 1;
 
 const TimerBar: React.FC<TimerBarProps> = ({
   duration,
@@ -14,30 +14,33 @@ const TimerBar: React.FC<TimerBarProps> = ({
   isTimeCut,
 }) => {
   const [remainingDuration, setRemainingDuration] = useState(duration);
-  const [animationDuration, setAnimationDuration] = useState(duration);
+  const [isCutActive, setIsCutActive] = useState(isTimeCut);
 
   useEffect(() => {
-    if (isTimeCut) {
-      const halfRemainingTime = Math.max(remainingDuration / 2, MIN_DURATION);
-      setRemainingDuration(halfRemainingTime);
-      setAnimationDuration(halfRemainingTime);
+    // Time-Cutter 사용 시
+    if (isTimeCut && !isCutActive) {
+      const newDuration = Math.max(remainingDuration / 2, MIN_DURATION);
+      setRemainingDuration(newDuration);
+      setIsCutActive(true);
 
       const timer = setTimeout(() => {
         onComplete();
-      }, halfRemainingTime * 1000);
-
-      return () => clearTimeout(timer);
-    } else {
-      setRemainingDuration(duration);
-      setAnimationDuration(duration);
-
-      const timer = setTimeout(() => {
-        onComplete();
-      }, duration * 1000);
+      }, newDuration * 1000);
 
       return () => clearTimeout(timer);
     }
-  }, [isTimeCut, duration, onComplete]);
+
+    // Time-Cutter가 트리거되지 않은 기본 타이머
+    const timer = setTimeout(() => {
+      onComplete();
+    }, remainingDuration * 1000);
+
+    return () => clearTimeout(timer);
+  }, [isTimeCut, onComplete, remainingDuration]);
+
+  // TODO
+  // TimeBar를 현재 시간 부터 5분으로 아이템을 사용하면 다른 부분에서 시간이 계속 바뀌면서 다른 버튼에도 영향을 가는 것으로 추측
+  // 현재 시간이 아닌 게임이 진행 시작 시간을 기준으로 작업하는게 좋아 보임.
 
   return (
     <div className="flex items-center gap-x-[15px]">
@@ -59,10 +62,11 @@ const TimerBar: React.FC<TimerBarProps> = ({
           }`}
           style={{
             width: '100%',
-            animation: `shrink ${animationDuration}s linear forwards`,
+            animation: `shrink ${remainingDuration}s linear forwards`,
           }}
         />
       </div>
+      <span>{remainingDuration}</span>
       <style jsx>{`
         @keyframes shrink {
           from {
