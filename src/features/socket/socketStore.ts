cf60f75a4db4f928718a09bc7df7a33386fca036
jeongminDev <bjm4256@gmail.com>
2024-11-24 18:55:08 +0900

    [FEAT] 단어 선택 시간 효과음 #47

--- src/features/socket/socketStore.ts
+++ src/features/socket/socketStore.ts
@@ -1,5 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 import { create } from 'zustand';
+import playSound from '../../utils/helpers/playSound';
 
 interface Participant {
   nickname: string;
@@ -63,6 +64,9 @@ const useSocketStore = create<SocketStore>((set, get) => ({
 
     socket.on('gameStateUpdate', (gameState: GameState) => {
       set({ gameState });
+      if (gameState.gameStatus === 'choosing') {
+        playSound('/sounds/fiveSecondSound.mp3');
+      }
     });
 
     set({ socket });
